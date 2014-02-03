a3936a6a54a829f30d85aea69923c0a3525df6c7
Igor Babuschkin <igor@babuschk.in>
2014-02-03 22:05:47 +0100

    Add git support to init.py

--- init.py
+++ init.py
@@ -1,10 +1,50 @@
 
+import sys
 import os
 
-try: input = raw_input
-except NameError: pass
+# Python 2 compatibility
+try:
+    input = raw_input
+except NameError:
+    pass
 
-name = input("Please enter the name of your project: ")
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is one of "yes" or "no".
+    """
+    valid = {"yes":True,   "y":True,  "ye":True,
+             "no":False,     "n":False}
+    if default == None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "\
+                             "(or 'y' or 'n').\n")
+
+name = input("Please enter a name for your project: ")
+use_git = query_yes_no("Do you want to initialize git?")
+git_url = ""
+if use_git:
+    git_url = input("Please enter the url of your git repository: ").strip()
 
 def template(name, target, **kwargs):
     f = open(name)
@@ -16,7 +56,14 @@ def template(name, target, **kwargs):
     f.write(text)
     f.close()
 
+print('Creating CMake files...')
 if name.strip():
     template('cmake/template.txt', 'CMakeLists.txt', project_name=name)
     template('cmake/template_src.txt', 'src/CMakeLists.txt', project_name=name)
+    template('cmake/template_test.txt', 'test/CMakeLists.txt', project_name=name)
+
+if git_url:
+    print('Initializing git...')
+    os.system('git remote rm origin > /dev/null 2>&1 || git init > /dev/null 2>&1')
+    os.system('git remote add origin ' + git_url)
 
